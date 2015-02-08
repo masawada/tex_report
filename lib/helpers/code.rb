6b354618bd76249464fa1cad9a154540f005906f
Masayoshi Wada <developer@andantesoftware.com>
2015-02-08 15:15:57 +0900

    コードのファイルコピー失敗する不具合を修正

--- lib/helpers/code.rb
+++ lib/helpers/code.rb
@@ -1,4 +1,5 @@
 def code(name, caption, label=nil, lang="C")
   label = name.gsub('.',  '-') if label.nil?
-  "\\lstinputlisting[caption=#{caption},label=#{label},language=#{lang}]{codes/#{name}}"
+  caption = caption.gsub('_', '\\_') unless caption.nil?
+  "\\lstinputlisting[caption=#{caption},label=#{label},language=#{lang}]{src/#{name}}"
 end
--- lib/tasks/codes.rb
+++ lib/tasks/codes.rb
@@ -2,7 +2,7 @@ namespace :codes do
   desc "src以下をtmpに移動します"
   task :copy do
     begin
-      sh "cp -r #{CODES} #{TMP}/codes"
+      sh "cp -r #{CODES} #{TMP}/"
     rescue
       puts "info: skip copy sourcecodes"
     end
